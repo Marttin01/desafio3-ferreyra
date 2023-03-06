41f9f9e262ac3605db21ec28166cf83a3557fa82
Marttin01 <martin.xd@hotmail.com.ar>
2023-03-06 13:32:10 -0300

    terminado

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import crypto, { randomUUID } from 'crypto'
+import { Product } from './Product.js'
+import { ProductManager } from './ProductManager.js'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const pm = new ProductManager('./static/products.json')
+
+app.get('/products', async (req,res) =>{
+    const products = await pm.getProducts()
+
+    const limit = req.query.limit
+    if(limit){
+        res.json(products.slice(0, limit))
+    }else {
+        res.json(products)
+    }
+})
+
+app.get('/products/:pid', async (req,res) => {
+    const product = await pm.getProductById(req.params.pid)
+    res.json(product)
+})
+
+app.post('/products', async (req,res) => {
+    let id = randomUUID()
+    const product = new Product ({
+        id:id,
+        ...req.body
+    })
+
+    
+    let agregado = await pm.addProduct(product)
+    res.json(agregado)
+})
+
+app.listen(8080, () => console.log('Servidor funcionando en 8080'))
